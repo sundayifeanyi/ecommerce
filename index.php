aa35d584b3650e6c4a6e99017eeb61f9588dd550
sundayifeanyi <thekingdomsunday@yahoo.com>
2020-08-12 05:23:23 +0100

    addedfooter

--- index.php
+++ index.php
@@ -36,19 +36,60 @@
         </div>
     </nav>
     <div id="background">
-    <div id="image1">
-    Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+    <div id="image1"> </div>
+    <div id="image2">  </div>
     </div>
-    <div id="image2">
-    Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+    <!-- featured products -->
+    <div class="row">
+    <h2 class="text-center">featured products</h2>
+    <div class="col-md-3">
+    <h4>jeans</h4>
+    <img src="images/lapback.jpg" alt="lappy" id="image" width="200px">
+    <p class="price_list text-danger">Price: <s>$24</s></p>
+    <p class="price ">Price: <s>$19.99</s></p>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#detials">details</button>
+    </div>
+    <div class="col-md-3">
+    <h4>shirt</h4>
+    <img src="images/lapback.jpg" alt="shirt" id="image" width="200px">
+    <p class="price_list text-danger">Price: <s>$24</s></p>
+    <p class="price ">Price: <s>$19.99</s></p>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#detials">details</button>
+    </div>
+    <div class="col-md-3">
+    <h4>mtn</h4>
+    <img src="images/lapback.jpg" alt="mtn" id="image" width="200px">
+    <p class="price_list text-danger">Price: <s>$24</s></p>
+    <p class="price ">Price: <s>$19.99</s></p>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#detials">details</button>
+    </div>
+    <div class="col-md-3">
+    <h4>glo</h4>
+    <img src="images/lapback.jpg" alt="glo" id="image" width="200px">
+    <p class="price_list text-danger">Price: <s>$24</s></p>
+    <p class="price ">Price: <s>$19.99</s></p>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#detials">details</button>
+    </div>
+    <div class="col-md-3">
+    <h4>etsalat</h4>
+    <img src="images/lapback.jpg" alt="etsalat" id="image" width="200px">
+    <p class="price_list text-danger">Price: <s>$24</s></p>
+    <p class="price ">Price: <s>$19.99</s></p>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#detials">details</button>
+    </div>
+    <div class="col-md-3">
+    <h4>airtel</h4>
+    <img src="images/lapback.jpg" alt="airtel" id="image" width="200px">
+    <p class="price_list text-danger">Price: <s>$24</s></p>
+    <p class="price ">Price: <s>$19.99</s></p>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#detials">details</button>
     </div>
     </div>
+    <footer class="text-center"><small>&copy; Copyright 2020-2021, ecommerce Corporation</small></footer>
+    </div>
+   
 
 </body>
 </html>
